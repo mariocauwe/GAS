f0d0f5cd97e94dddba11d1fb52b4d62c18856511
mariocauwe <mariocauwe@gmail.com>
2024-01-10 00:05:20 +0100

    Add files via upload
    print headers

--- app.js
+++ app.js
@@ -11,8 +11,18 @@ app.get('/test',(request, response) => {
     logger.info(request.query)
     response.send('/test')
   })
+
+  /**
+   * X-Goog-Channel-ID: channel-ID-value
+X-Goog-Channel-Token: channel-token-value
+X-Goog-Channel-Expiration: expiration-date-and-time // In human-readable format. Present only if the channel expires.
+X-Goog-Resource-ID: identifier-for-the-watched-resource
+X-Goog-Resource-URI: version-specific-URI-of-the-watched-resource
+X-Goog-Resource-State: sync
+X-Goog-Message-Number: 1
+   */
 app.post('/gas',(request, response) => {
-    logger.info('POST /gas', request.body)
+    logger.info('POST /gas', request.get('X-Goog-Channel-Token'), request.get('X-Goog-Resource-ID'))
     response.send('/gas')
   })
 
